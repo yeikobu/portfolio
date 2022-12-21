7f6b487babf3cdeb656aa22b080aef97f16d9659
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2022-12-21 18:56:50 -0300

    About me has been modified

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,12 +6,12 @@ const About = () => {
     <>
       <section className='profile-container' alt='Jacob Aguilar'>
         <aside className='resume-container' >
-          <h3> Hi! I'm Jacob Aguilar👋🏼</h3>
-          <p> Software developer</p>
+          <h3> Hi! I'm Jacob Aguilar👋🏼 </h3>
+          <p> iOS Developer</p>
           <h1> "Solving problems by tech is my passion" </h1>
           <div className='sns-container'>
-            <a href='https://www.instagram.com/dev.swift.jacob/'>
-              <img src='https://firebasestorage.googleapis.com/v0/b/portfolio-c3ddb.appspot.com/o/about%2Finstagram.png?alt=media&token=f8f330d2-7dea-409b-ade5-09521bf6b0b4' className='sns-icon' alt='Jacob Aguilar Instagram' />
+            <a href='https://twitter.com/yeikobu_'>
+              <img src='https://firebasestorage.googleapis.com/v0/b/portfolio-c3ddb.appspot.com/o/about%2Ftwitter.png?alt=media&token=51852a80-d7fa-4f29-9a7d-bec97a02f9a1' className='sns-icon' alt='Jacob Aguilar Instagram' />
             </a>
             <a href="https://www.linkedin.com/in/jacob-aguilar-campos-4b3406a4/">
               <img src='https://firebasestorage.googleapis.com/v0/b/portfolio-c3ddb.appspot.com/o/about%2Flinkedin.png?alt=media&token=91eeb5ce-0d6a-4537-94d1-4dc3284a1ae8' className='sns-icon' alt='Jacob Aguilar Likedin' />
@@ -27,13 +27,17 @@ const About = () => {
       </section>
       <section>
         <div className='about-me'>
-          <h3 className='about-me-title' > About me</h3>
-          <p className='about-me-text'>I'm a Software Engineer student based in Santiago, Chile.
-            Since 2020 I started to learn programming at university.
-            At present, I provide solutions with Swift and Javascript to many clients around the world. 
+          <h3 className='about-me-title' >About me</h3>
+          <p className='about-me-text'>I am a Software development student based in Santiago de Chile.
             <br />
             <br />
-            Now I'm a Student on <a href='https://platzi.com/r/j-kobu'>Platzi</a>
+            Currently, I am in the process of growing as a person by studying business and finance, as well as software development and improving my soft skills on <a href='https://platzi.com/r/yeikobu'>Platzi</a>. 
+            <br />
+            <br />
+            I have created native mobile projects for Android and iOS. I have also developed small web projects.
+            <br />
+            <br />
+            I like inline skating, and skates become my main transportation when I'm in the city. So if I show up to a meeting sweaty, you know why.
           </p>
         </div>
       </section>
